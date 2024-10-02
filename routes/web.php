0834d5d507b58ca783c4f378e0b471e4872698be
Anthony DeVenuto <adevenuto@macbook-m1.local>
2024-10-01 21:34:21 -0500

    stubbing things out for posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,12 +22,17 @@ Route::get('/blog', function () {
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
   Route::get('/dashboard', function () {
-      return view('dashboard');
+      return view('dashboard.index');
   })->name('dashboard');
 
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+  Route::get('/posts', [PostController::class, 'index'])->name('posts');
+  // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 
